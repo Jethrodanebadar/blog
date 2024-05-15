47c659e692f8a170ec1594b48accb80bb79e4cf9
Badar <jbadar@bju.edu>
2024-05-15 16:59:33 -0400

    may 15 accomplishment

--- personalblog/home/models.py
+++ personalblog/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class blog_data(models.Model):
+    title = models.CharField(max_length=100)
+    author = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    content = models.TextField()
+    image = models.ImageField()
+    
